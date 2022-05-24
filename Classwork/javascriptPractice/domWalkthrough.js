66c130ca6fb7f10ea5cad053c0da40fa58d712ef
TMerco <Tmercolino@sea.yearup.org>
2022-05-24 16:02:16 -0700

    sdasa

--- Classwork/javascriptPractice/domWalkthrough.js
+++ Classwork/javascriptPractice/domWalkthrough.js
@@ -1,5 +1,13 @@
 //console.dir(document);
 
-console.log(document.domain);
-
+console.log(document.domain); //<---shows IP address
 console.log(document.URL);
+
+// access the title element
+console.log(document.title);
+
+// check the doc type (HTML)
+console.log(document.doctype);
+
+//can see everything in the "head"
+console.log(document.head);
