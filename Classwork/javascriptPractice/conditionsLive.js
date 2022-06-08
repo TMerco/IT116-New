59452bc48e5e17d1efdf4ea96fdcc8ca2e8b4875
TMerco <Tmercolino@sea.yearup.org>
2022-06-08 13:36:23 -0700

    JS

--- Classwork/javascriptPractice/conditionsLive.js
+++ Classwork/javascriptPractice/conditionsLive.js
@@ -5,123 +5,123 @@ let randomNumber = Math.random();
 
 console.log(randomNumber); 
 
-// if (randomNumber < 0.5) {
+if (randomNumber < 0.5) {
 
-//     console.log("Hello Monte");
-//     console.log(randomNumber);
+    console.log("Hello Monte");
+    console.log(randomNumber);
 
-// }
+}
 
-// if (randomNumber >= 0.5) {
+if (randomNumber >= 0.5) {
 
-//     console.log('Random Number is greater than 0.5');
-//     console.log(randomNumber);
+    console.log('Random Number is greater than 0.5');
+    console.log(randomNumber);
 
-// }
+}
 
-//If Else Statement:
-
-//
-
-// const dayOfWeek = 'Monday';
-
-// if (dayOfWeek === 'Monday') {
-
-//     console.log('This is Monday');
-
-// } else if (dayOfWeek === 'Thursday') {
-
-//     console.log("This is Thursday");
-
-// } else if (dayOfWeek ===  'Saturday') {
-
-//     console.log("Hooray, today is Saturday");
-// }
+If Else Statement:
 
 
-// const age = prompt('Enter An Age To Drive!');
+
+const dayOfWeek = 'Monday';
+
+if (dayOfWeek === 'Monday') {
+
+    console.log('This is Monday');
+
+} else if (dayOfWeek === 'Thursday') {
+
+    console.log("This is Thursday");
+
+} else if (dayOfWeek ===  'Saturday') {
+
+    console.log("Hooray, today is Saturday");
+}
 
 
-// if (age < 5) {
-
-//     console.log("You are less than 5! Cmon bro.");
-// } else if (age < 10) {
-
-//     console.log("Still too young!");
-
-// } else if (age >= 16) {
-
-//     console.log("You are able to drive!");
-// }
-
-// const age = prompt('ENTER AN AGE');
-// // const age = 8;
-// // Relational opperators //
-// if (age < 5) {
-//     console.log("You Type Pretty Fast - 5 ");
-// } else if (age < 10) {
-//     console.log("Output of the logic compared to CASE STATEMENTS");
-// } else if (age < 35) {
-//     console.log("Output of the logic Mr Adult.");
-// }
-// //ELSE -- ALL ALL CONDITIONS IN THE LOGIC HAVE FAILED
-// else {
-
-//     console.log("Last Resort If All Conditons Fail");
-// } 
+const age = prompt('Enter An Age To Drive!');
 
 
-// //switch statement allows to just write cases instead//
-// const expr = 'Oranges' ;
+if (age < 5) {
 
-// switch (expr) {
+    console.log("You are less than 5! Cmon bro.");
+} else if (age < 10) {
 
-//     case 'Supreme':
-//         console.log("Supreme is a skater brand.");
-//         break;
+    console.log("Still too young!");
 
-//     case 'Bape':
-//             console.log("Bape is a japanese brand");
-//             break;
+} else if (age >= 16) {
 
-//     case 'OFF-White':
-//         console.log("OFF-White is Virgil's brand");
-//         break;
-// //This is h
-//         default:
-//             console.log("CAWZY is the best brand though");
+    console.log("You are able to drive!");
+}
+
+const age = prompt('ENTER AN AGE');
+const age = 8;
+Relational opperators //
+if (age < 5) {
+    console.log("You Type Pretty Fast - 5 ");
+} else if (age < 10) {
+    console.log("Output of the logic compared to CASE STATEMENTS");
+} else if (age < 35) {
+    console.log("Output of the logic Mr Adult.");
+}
+//ELSE -- ALL ALL CONDITIONS IN THE LOGIC HAVE FAILED
+else {
+
+    console.log("Last Resort If All Conditons Fail");
+} 
 
 
-// }
+//switch statement allows to just write cases instead//
+const expr = 'Oranges' ;
 
-// const password = prompt("Please Enter A New Password");
+switch (expr) {
 
-// if (password.length >= 6) {
+    case 'Supreme':
+        console.log("Supreme is a skater brand.");
+        break;
 
-//     //Nested Condition
-//     if (password.indexOf(' ') === -1) {
+    case 'Bape':
+            console.log("Bape is a japanese brand");
+            break;
 
-//         console.log("Valid Password")
-//     }else {
-//         console.log("Password Can't Have Spaces")
-//     }
+    case 'OFF-White':
+        console.log("OFF-White is Virgil's brand");
+        break;
 
-// } else {
+        default:
+            console.log("CAWZY is the best brand though");
 
-//     console.log("Password is Too Short")
-// }
 
-// function singSong() {
-//     console.log("DO");
-//     console.log("RE");
-//     console.log("MI");
-// }
+}
 
-// singSong()
-// singSong()
-// singSong()
+const password = prompt("Please Enter A New Password");
 
-//Templete Literals//
+if (password.length >= 6) {
+
+    //Nested Condition
+    if (password.indexOf(' ') === -1) {
+
+        console.log("Valid Password")
+    }else {
+        console.log("Password Can't Have Spaces")
+    }
+
+} else {
+
+    console.log("Password is Too Short")
+}
+
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong()
+singSong()
+singSong()
+
+// Templete Literals
 function greet(firstName) {
 
     console.log(`Hey There, ${firstName}`)
